e54f3ce10d850f65400a7f149e797572d07ea843
Ryan Castro Ferreira <ryancasf@yahoo.com>
2019-09-06 15:15:48 -0300

    Atualização de menssagem do tipo Json

--- aluno.js
+++ aluno.js
@@ -14,5 +14,11 @@ window.onload = function() {
         console.log(botao);
         let botaoQGira = (Ladda.create(botao[0]));
         botaoQGira.start();
+
+        fetch("http://localhost:8080/sen-api/test.php").then(function(resposta) {
+            return resposta.json();
+        }).then(function(dados){
+            alert(dados.menssagem);
+        } );
     });
 }
